7d4774d763afa5ed8d4f98b339620bde478a3c9d
richiedlr <rdelarosa1@ucol.mx>
2025-09-11 12:02:33 -0600

    interfaz pantalla de espera

---
+++ pantalla_espera.php
@@ -0,0 +1,41 @@
+<?php
+// Aquí podrías agregar lógica PHP, por ejemplo:
+// session_start();
+// include("conexion.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Círculos entrelazados</title>
+  <link rel="stylesheet" href="css/components/pantalla_espera.css">
+</head>
+<body>
+
+<header>
+    <div class="logo">MiLogo</div>
+    <nav class="menu">
+      <a href="#">Inicio</a>
+      <a href="#">Servicios</a>
+      <a href="#">Nosotros</a>
+      <a href="#">Contacto</a>
+    </nav>
+  </header>
+
+  <main>
+  </main>
+
+  <div class="texto">TOMA TU TURNO</div>
+
+  <div class="contenedor">
+    <div class="circulo rojo"></div>
+    <div class="circulo azul"></div>
+    <div class="circulo verde"></div>
+
+    <button class="boton izquierda">CLIENTE</button>
+    <button class="boton derecha">VISITANTE</button>
+  </div>
+
+</body>
+</html>
