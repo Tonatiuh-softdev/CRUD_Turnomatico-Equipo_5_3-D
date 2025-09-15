d55a07a7a4a59c0270e4631efdcd6f2728b79655
chenaoh <lapijamarosadesuga626@gmail.com>
2025-09-14 22:20:22 -0600

    hice la base de la interfaz de la pantalla de Turnos y acomode la hora para que se actualice pero solo en la pantalla de turnos

--- index.php
+++ index.php
@@ -167,7 +167,7 @@ require 'navbar.php';
     <img src="https://img.icons8.com/ios-filled/50/000000/conference.png"/>
     Pantalla de espera
   </a>
-  <a href="pantalla_turno.php" class="card">
+  <a href="pantallaDeTurno.php" class="card">
     <img src="https://img.icons8.com/ios-filled/50/000000/return.png"/>
     Pantalla de turno
   </a>
