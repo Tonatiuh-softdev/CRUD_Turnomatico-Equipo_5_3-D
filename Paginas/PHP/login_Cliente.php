4b804e883c9d3c6f9ae02ecc0e1038894a985f16
Pablo Alcala <148588239+AlkaDev1@users.noreply.github.com>
2025-10-21 18:28:38 -0600

    Se movio el archivo login_Cliente.css a Paginas/CSS/ y se actualizaron las referencias

--- Paginas/PHP/login_Cliente.php
+++ Paginas/PHP/login_Cliente.php
@@ -43,7 +43,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Login Cliente</title>
-<link rel="stylesheet" href="../../css/components/login_Cliente.css">
+<link rel="stylesheet" href="../CSS/login_Cliente.css">
 </head>
 <body>
 <img src="../../img/fondo_login.png" alt="Fondo" class="imagen">
