38c85a13a3bfa9b57db9ad0bed0cab4ddd55d2c5
Pablo Alcala <148588239+AlkaDev1@users.noreply.github.com>
2025-10-22 18:14:52 -0600

    Se creo el archivo header.html y se agrego a header.php

--- Paginas/Componentes/PHP/header.php
+++ Paginas/Componentes/PHP/header.php
@@ -3,19 +3,7 @@ date_default_timezone_set("America/Mexico_City");
 $hora = date("h:i a");
 setlocale(LC_TIME, "es_MX.UTF-8"); 
 $fecha = (new DateTime())->format("d \ \e F \ \e Y");
+
+// Incluir el HTML del header
+include __DIR__ . "/../HTML/header.html";
 ?>
-
-<body>
-    
-
-<header>
-        <div class="empresa">
-            <img src="../../../img/img.Logo_blanco.png" alt="logo">
-            ClickMatic
-        </div>
-        <div class="info">
-            <?php echo $hora; ?><br>
-            <?php echo ucfirst($fecha); ?>
-        </div>
-</header>
-</body>
